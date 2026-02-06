52b97c3b16826453e3a9b20ea1df503a3dda7421
krishanveer191 <krishnveer1822005@gmail.com>
2026-02-06 11:24:40 +0530

    [Krishanveer file operation assign].file-opern assign

---
+++ nodejs/file.js
@@ -0,0 +1,12 @@
+// import express from 'express'
+// const app=express()
+// fs.existsSync("todo.json")it check the file it exists or not
+// fs.writeFileSync("todo.json",JSON.stringify(data,null,2)); write file and pass custom data by json.stringify
+// fs.readFileSync("todo.json","utf-8") simple read file data
+// let data=JSON.parse(fs.readFileSync("todo.json","utf-8")); it take json data
+// fs.readFileSync("assignment.txt","utf-8",(err,data)={ it read file but in express we cant pass file this we want absolute path of file and pass the absoulte path in response
+//     if(err){
+
+//         response.send("some massage")
+//     }
+// })
